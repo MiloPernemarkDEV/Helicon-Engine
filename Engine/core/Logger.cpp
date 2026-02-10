a38b9679ac43897d48cdb97b6dd1eba1a58d6069
MiloPernemarkDEV <milo.pernemark@student.forsbergsskola.se>
2026-02-10 11:26:59 +0100

    formatted switch statement in Logger.cpp

--- Engine/core/Logger.cpp
+++ Engine/core/Logger.cpp
@@ -30,26 +30,28 @@ namespace Helicon
         HEL_DEBUG("Logger Init called");
     }
 
-        void Logger::HLog(LogLevel level, const char* message, const char* file, int line) {
-    const char* colorReset = "\x1B[0m";
-    const char* colorRed   = "\x1B[31m";
-    const char* colorGreen = "\x1B[32m";
-    const char* colorYellow= "\x1B[33m";
-    const char* colorCyan  = "\x1B[36m";
+    void Logger::HLog(LogLevel level, const char* message, const char* file, int line) 
+    {
+        const char* colorReset = "\x1B[0m";
+        const char* colorRed   = "\x1B[31m";
+        const char* colorGreen = "\x1B[32m";
+        const char* colorYellow= "\x1B[33m";
+        const char* colorCyan  = "\x1B[36m";
 
-    switch (level) {
-        case LogLevel::Debug:   
-            PLOGD << file << ":" << " " << colorCyan << line << "." << " " << message << colorReset;
-            break;
-        case LogLevel::Info:    
-            PLOGI << file << ":" << " " << colorGreen << line << "." << " " << message << colorReset;
-            break;
-        case LogLevel::Warning: 
-            PLOGW << file << ":" << " " << colorYellow << line << "." << " " << message << colorReset;
-            break;
-        case LogLevel::Error:   
-            PLOGE << file << ":" << " " << colorRed << line << "." << " " << message << colorReset;
-            break;
+        switch (level) 
+        {
+            case LogLevel::Debug:   
+                PLOGD << file << ":" << " " << colorCyan << line << "." << " " << message << colorReset;
+                break;
+            case LogLevel::Info:    
+                PLOGI << file << ":" << " " << colorGreen << line << "." << " " << message << colorReset;
+                break;
+            case LogLevel::Warning: 
+                PLOGW << file << ":" << " " << colorYellow << line << "." << " " << message << colorReset;
+                break;
+            case LogLevel::Error:   
+                PLOGE << file << ":" << " " << colorRed << line << "." << " " << message << colorReset;
+                break;
+        }
     }
-}
 }
